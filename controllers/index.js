70261cb8ec3184f5b9e039ccb451216c74a315bd
LaoHu97 <962271461@qq.com>
2017-12-09 10:20:09 +0800

    2017-12-09

--- controllers/index.js
+++ controllers/index.js
@@ -8,76 +8,42 @@ var ROOTLOGO = require("../models/rootLogo.js");
 var pool = mysql.createPool(util.extend({}, db.mysql));
 
 
-
 async function logo(req, res, next) {
   const {username, password} = req.body;
-  try {
-    pool.getConnection(function(err, connection) {
-      connection.query(ROOTLOGO.adminByusername, username, function(err, result) {
-        if (result[0] == undefined) {
-          res.send({
-            status: 300,
-            message: '帐号不存在！'
-          })
-        } else {
-          if (result[0].password === password) {
-            res.send({
-              status: 200,
-              message: '登录成功！'
-            })
-          } else {
-            res.send({
-              status: 300,
-              message: '密码错误！'
-            })
-          }
-        };
-        connection.release();
-      });
-    });
-  } catch (err) {
+  const admin_id = req.session.admin_id;
+  if (admin_id) {
     res.send({
-      status: 400,
-      message: '登录失败!'
+      status: 200,
+      message: '登录成功！'
     })
+  }else {
+    try {
+      const admin = await ROOTLOGO.findOne({username});
+      if (!admin) {
+        res.send({
+          status: 300,
+          message: '帐号不存在！'
+        })
+      }else if (password!=admin.userpwd) {
+        res.send({
+          status: 300,
+          message: '密码错误！'
+        })
+      }else {
+        req.session.admin_id = admin.id;
+        res.send({
+          status: 200,
+          message: '登录成功！'
+        })
+      }
+    } catch (err) {
+      res.send({
+        status: 400,
+        message: '登录失败!'
+      })
+    }
   }
 }
-// async function logo(req, res, next) {
-//   const {username, password} = req.body;
-//   const admin_id = req.session.admin_id;
-//   if (admin_id) {
-//     res.send({
-//       status: 200,
-//       message: '登录成功！'
-//     })
-//   }else {
-//     try {
-//       const admin = await ROOTLOGO.findOne({username});
-//       if (!admin) {
-//         res.send({
-//           status: 300,
-//           message: '帐号不存在！'
-//         })
-//       }else if (password!=admin.userpwd) {
-//         res.send({
-//           status: 300,
-//           message: '密码错误！'
-//         })
-//       }else {
-//         req.session.admin_id = admin.id;
-//         res.send({
-//           status: 200,
-//           message: '登录成功！'
-//         })
-//       }
-//     } catch (err) {
-//       res.send({
-//         status: 400,
-//         message: '登录失败!'
-//       })
-//     }
-//   }
-// }
 module.exports = {
   logo: logo
 };
