452d319438bc0019f44d21c3932f012be5d05f94
LaoHu97 <962271461@qq.com>
2018-04-16 11:35:50 +0800

    2018-4-16

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('view engine', 'jade');
 
 // //设置跨域访问
 app.all('*', (req, res, next) => {
-	res.header("Access-Control-Allow-Origin", req.headers.origin || '*');
+	res.header("Access-Control-Allow-Origin", req.headers.origin);
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Credentials", true); //可以带cookies
